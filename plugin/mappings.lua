26de1801e454a9bce92c679aa06603b73f4f6813
beppune <manzo.giuseppe@gmail.com>
2025-05-20 18:07:23 +0200

    add <Plug> mappings and eager require

---
+++ plugin/mappings.lua
@@ -0,0 +1,5 @@
+local M = require 'tsmotions'
+
+-- <Plugin> mappings for tsmotions
+vim.keymap.set('n', '<Plug>(MoveToNextId)', ':lua package.loaded.tsmotions.NextId()<Enter>', { noremap = true }   )
+vim.keymap.set('n', '<Plug>(MoveToPrevId)', ':lua package.loaded.tsmotions.PrevId()<Enter>', { noremap = true }   )
